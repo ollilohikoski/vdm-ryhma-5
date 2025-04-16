d645f9f117a2a7fc23f6a2ec2486722d24686bb0
lohikoo2 <olli.lohikoski@aalto.fi>
2025-04-16 12:32:01 +0300

    Fixed header heigh on mobile

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
       <div className='h-[200vh]'>
         <Header />
-        <div className='max-w-4xl w-full mx-auto flex gap-8 mt-36'>
+        <div className='max-w-4xl w-full mx-auto flex gap-8 mt-58 md:mt-36'>
           <h1>Text</h1>
         </div>
       </div>
