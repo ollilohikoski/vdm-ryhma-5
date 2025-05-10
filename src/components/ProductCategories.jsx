dd2bc936bf4a3af19ddefe72595acda463132173
lohikoo2 <olli.lohikoski@aalto.fi>
2025-05-10 15:11:24 +0300

    Added images, fixed font

--- src/components/ProductCategories.jsx
+++ src/components/ProductCategories.jsx
@@ -1,24 +1,36 @@
 import "./ProductCategories.css";
 import jumpsuit from "../assets/product/jumpsuit.png";
-
-const placholderImage = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQye9kPfMG92VKMdNp7Drx_xBGVh6UOeOG6ig&s";
+import hot1 from "../assets/product/hot1.png";
+import men from "../assets/product/men.png";
+import kids from "../assets/product/kids.png";
+import curve from "../assets/product/curve.png";
+import hot2 from "../assets/product/hot2.png";
+import hot3 from "../assets/product/hot3.png";
+import tops from "../assets/product/tops.png";
+import bottoms from "../assets/product/bottoms.png";
+import suits from "../assets/product/suits.png";
+import lingerie from "../assets/product/lingerie.png";
+import jeans from "../assets/product/jeans.png";
+import shoes from "../assets/product/shoes.png";
+import hot4 from "../assets/product/hot4.png";
+import home from "../assets/product/home.png";
 
 const categories = [
-  { title: "Women", imgSrc: placholderImage, emphasize: true },
-  { title: "Men", imgSrc: placholderImage },
-  { title: "Kids", imgSrc: placholderImage },
-  { title: "Curve", imgSrc: placholderImage },
-  { title: "Dresses", imgSrc: placholderImage, emphasize: true },
-  { title: "Beachwear", imgSrc: placholderImage, emphasize: true },
-  { title: "Tops", imgSrc: placholderImage },
+  { title: "Women", imgSrc: hot1, emphasize: true },
+  { title: "Men", imgSrc: men },
+  { title: "Kids", imgSrc: kids },
+  { title: "Curve", imgSrc: curve },
+  { title: "Dresses", imgSrc: hot2, emphasize: true },
+  { title: "Beachwear", imgSrc: hot3, emphasize: true },
+  { title: "Tops", imgSrc: tops },
   { title: "Jumpsuits & Co-ords", imgSrc: jumpsuit },
-  { title: "Bottoms", imgSrc: placholderImage },
-  { title: "Suits", imgSrc: placholderImage },
-  { title: "Lingerie & Loungewear", imgSrc: placholderImage },
-  { title: "Jeans", imgSrc: placholderImage },
-  { title: "Shoes", imgSrc: placholderImage },
-  { title: "Beauty & Health", imgSrc: placholderImage, emphasize: true },
-  { title: "Home & Living", imgSrc: placholderImage },
+  { title: "Bottoms", imgSrc: bottoms },
+  { title: "Suits", imgSrc: suits },
+  { title: "Lingerie & Loungewear", imgSrc: lingerie },
+  { title: "Jeans", imgSrc: jeans },
+  { title: "Shoes", imgSrc: shoes },
+  { title: "Beauty & Health", imgSrc: hot4, emphasize: true },
+  { title: "Home & Living", imgSrc: home },
 ];
 
 export default function ProductCategories() {
